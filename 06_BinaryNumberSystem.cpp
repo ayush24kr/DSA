7d092c2ade5cff6d08368eab7cc06d83254fa3d7
ayush24kr <ayush24kr@gmail.com>
2025-07-17 17:59:47 +0530

    updated 06_BinaryNumberSystem

--- 06_BinaryNumberSystem.cpp
+++ 06_BinaryNumberSystem.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int dectobin(int dec){
-    int ans=0 , power =1;
-    while( dec > 0) {
-        int rem = dec% 2;
-        dec /= 2;
+// int dectobin(int dec){
+//     int ans=0 , power =1;
+//     while( dec > 0) {
+//         int rem = dec% 2;
+//         dec /= 2;
 
-        ans += (rem * power);
-        power *= 10;
+//         ans += (rem * power);
+//         power *= 10;
+//     }
+//     return ans;
+// }
+
+int bintodec(int binnum){
+    int ans =0 ,pow =1 ;
+    while (binnum >0 ){
+        int rem = binnum %10;
+        ans+=rem *pow;
+        binnum/=10;
+        pow*=2;
     }
     return ans;
 }
 
 int main(){
-    int dec=10;
-    cout << dectobin(dec) << endl; 
+    int dec=50;
+    // cout << dectobin(dec) << endl;
+    cout << bintodec(1010) << endl ; 
     return 0;
 }
