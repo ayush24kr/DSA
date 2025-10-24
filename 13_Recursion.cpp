92463dadf615f04367903b9eda6f96b88a1b42b1
Ayush <techarunprd@gmail.com>
2025-10-24 21:49:59 +0530

    Implement factorial function using recursion

---
+++ 13_Recursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    
+    // Base case
+    if (n == 0) {
+        return 1;
+    }
+    // Recursive case
+    return n * factorial(n - 1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int ans = factorial(n);
+    cout << ans << endl;
+    return 0;
+}
